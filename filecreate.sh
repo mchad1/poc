0b6d2e0d2f68d553fae4132955f18048a49afff6
root <root@mchad-southcentral-lv2-rhel78.ixpc2pkzeueexpnmkukkaicoze.jx.internal.cloudapp.net>
2020-09-14 09:09:25 +0000

    File Modified

--- filecreate.sh
+++ filecreate.sh
@@ -1,6 +1,6 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16; do
      for dir in /nfsv3/reads; do 
        mkdir -p /nfsv3/reads
-       taskset dd if=/dev/zero of=${dir}/file$i count=4194304 bs=64k &
+       dd if=/dev/zero of=${dir}/file$i count=4194304 bs=64k &
      done
 done
